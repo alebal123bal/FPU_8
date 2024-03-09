3d08e229f6a44cc2929b20c923bb9d87a3bbfde2
alebal123bal <balzanalessandro2001@gmail.com>
2024-03-09 15:07:49 +0100

    Initial commit of package for constant defs like inf, NaN etc.

---
+++ FPU_PACK.v
@@ -0,0 +1,13 @@
+package FPU_8;
+    //Define constants for M5E2 format FP8
+    `define _PLUS_INF 8'b01111100
+    `define _MINUS_INF 8'b11111100
+    `define _NAN 8'b01111101
+    `define _NAN 8'b01111110
+    `define _NAN 8'b01111111
+    `define _NAN 8'b11111101
+    `define _NAN 8'b11111110
+    `define _NAN 8'b11111111
+    `define _ZERO 8'b00000000
+    `define _ZERO 8'b10000000
+endpackage
