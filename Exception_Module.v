3f0fd652099d5ae793f38f92df83513390c71cc9
alebal123bal <balzanalessandro2001@gmail.com>
2024-03-09 15:07:07 +0100

    Changed logic of exception handling

--- Exception_Module.v
+++ Exception_Module.v
@@ -1,11 +1,14 @@
-module Exception_Module(FP_OPERATION, OP_IN, OP_IS_EXCEPTION):
+module Exception_Module(FP_OPERATION, OP_A, OP_B, OP_IS_EXCEPTION):
     input wire[1:0] FP_OPERATION;
-    input wire[7:0] OP_IN;
+    input wire[7:0]  OP_A, OP_B;
     output reg OP_IS_EXCEPTION;
 
     always @(*) begin
-        case (OP_IN)
-            2'b00: //To check input operands for excpt condition checking
+        case (FP_OPERATION)
+            //Addition exception: +inf +inf is undef
+            2'b00: OP_IS_EXCEPTION if (conditions) begin
+                
+            end
         endcase
 
     end
