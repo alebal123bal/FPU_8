33812c653d1d30bee51a1d7e6a1b48c7cbcf73e2
alebal123bal <balzanalessandro2001@gmail.com>
2024-03-18 19:23:21 +0100

    Added a simple run.ys

--- FPU_8.v
+++ FPU_8.v
@@ -1,15 +1,19 @@
 `include "EXCEPTION_MODULE.v"
 `include "FPU_PACK.v"
 
-module FPU_8(FP_Start, OP_A, OP_B, FP_OPERATION, FP_ROUND_MODE, OP_RESULT, FP_is_EXCEPTION, FP_Exception);
+module FPU_8(FP_Start, OP_A, OP_B, FP_OPERATION, FP_ROUND_MODE, OP_RESULT, OP_IS_EXCEPTION, FP_Exception);
     input wire FP_Start;
     input wire[7:0] OP_A, OP_B;
     input wire[1:0] FP_OPERATION;
     input wire FP_ROUND_MODE;
 
     output reg[7:0] OP_RESULT;
-    output reg FP_IS_EXCEPTION;
+    output reg OP_IS_EXCEPTION;
     output reg[1:0] FP_Exception;
 
     EXCEPTION_MODULE my_mod(FP_OPERATION, OP_A, OP_B, OP_IS_EXCEPTION);
+
+    always@(*) begin
+    end
+    
 endmodule
