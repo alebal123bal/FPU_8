9af59c6dfe03af0d60e9554456d81678bff175da
alebal123bal <balzanalessandro2001@gmail.com>
2024-03-18 18:00:32 +0100

    Module to check if any operand is zero; reusable like CHECK NAN

---
+++ CHECK_ZERO.v
@@ -0,0 +1,10 @@
+module CHECK_ZERO(fp8_in, is_Zero);
+    input wire[7:0] fp8_in;
+    output reg is_Zero;
+
+    always@(*) begin
+        is_Zero <= (fp8_in == `_ZERO_0) ||
+        (fp8_in == `_ZERO_1);
+    end
+
+endmodule
