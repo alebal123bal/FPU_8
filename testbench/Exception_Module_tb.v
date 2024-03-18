c4e2239618a5a27f8843aa9dddc871efa81d5128
alebal123bal <balzanalessandro2001@gmail.com>
2024-03-18 17:47:14 +0100

    Added waveform display directive

--- testbench/Exception_Module_tb.v
+++ testbench/Exception_Module_tb.v
@@ -3,12 +3,21 @@ module Exception_Module_tb();
     reg[7:0] in0;
     reg[7:0] in1;
 
-    //Output signal
-    wire out;
+    //Output signals
+    wire out0;
+    wire out1;
+    wire out2;
+    wire out3;
 
-    Exception_Module DUT (`_ADDITION, in0, in1, out);
+    Exception_Module DUT0 (`_ADDITION, in0, in1, out0);
+    Exception_Module DUT1 (`_SUBTRACTION, in0, in1, out1);
+    Exception_Module DUT2 (`_MULTIPLICATION, in0, in1, out2);
+    Exception_Module DUT3 (`_DIVISION, in0, in1, out3);
 
-  	initial begin    
+  	initial begin
+        $dumpfile("my_waveform.vcd");
+        $dumpvars(0, Exception_Module_tb);
+
         // Set input signals
         in0 = `_PLUS_INF;
         in1 = 8'b00000000;
@@ -17,10 +26,11 @@ module Exception_Module_tb();
         #10;
 
         // Check output signal: should yield exception true
-        assert (out === 1)
-          $display("Pass: Exception caught correctly");
+        assert (out0 === 1 && out1 === 1)
+          $display("Pass: Exceptions caught correctly");
         else
           $display("Fail: No exception caught");
+
 		#100 $finish;
     end
 endmodule;
