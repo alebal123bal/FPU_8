83035c3b144fac95443ba9c0b8cd5ec9a5625fb3
alebal123bal <balzanalessandro2001@gmail.com>
2024-04-17 12:11:21 +0200

    Splitted qNaN and sNaN

--- FPU_PACK.v
+++ FPU_PACK.v
@@ -7,12 +7,12 @@
 //Define constants for M5E2 format FP8
 `define _PLUS_INF 8'b01111100
 `define _MINUS_INF 8'b11111100
-`define _NAN_0 8'b01111101    //_SNaN
+`define _NAN_0 8'b01111101    //_SNaN (check the 2nd LSB)
 `define _NAN_1 8'b01111110    //_QNaN
-`define _NAN_2 8'b01111111
+`define _NAN_2 8'b01111111    //_QNaN
 `define _NAN_3 8'b11111101    //_SNaN
 `define _NAN_4 8'b11111110    //_QNaN
-`define _NAN_5 8'b11111111
+`define _NAN_5 8'b11111111    //_QNaN
 `define _ZERO_0 8'b00000000
 `define _ZERO_1 8'b10000000
 
